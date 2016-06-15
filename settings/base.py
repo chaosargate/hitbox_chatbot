e52f006070508613b1da1c0f33a900bec2d8cc00
Aaron <chaosargate@gmail.com>
2016-06-15 17:41:19 -0400

    Initial commit

---
+++ settings/base.py
@@ -0,0 +1,7 @@
+hitbox_api = 'https://api.hitbox.tv/{api}'
+server_list_path = 'chat/servers'
+auth_token_path = 'auth/token'
+
+http_head = 'http://'
+ws_head = 'ws://'
+socket_base = '/socket.io/1/'
